e4aece9444cb22cbbaf04fa2b7b7e81d152314f1
Elijah Houle <elijah.houle@gmail.com>
2014-09-05 18:27:50 -0700

    Add shot segmentation for keyframe extraction
    For each I-frame's blue, green, and red channels, a histogram is computed. Then each histogram is compared between
    frames--if the correlation is lower than a threshold, then the frames belong to different shots and are returned
    as keyframes.
    
    This should be made more efficient in the future by saving the histogram of keyframe[i+1] instead of calculating it twice.

--- process-video.py
+++ process-video.py
@@ -4,11 +4,47 @@
 
 import cv2
 import ffms
+import numpy as np
 import sys
 
+SHOT_CORREL_THRESHOLD = 0.7 # minimum color histogram correlation within shot
 
+
+def keyframe_index(vs, width, height, i):
+    return vs.get_frame(vs.track.keyframes[i]).planes[0].reshape((height, width, 3)).copy()
+
+
+def calc_histogram(frame):
+    hist = [] # list of histograms for all channels (colors)
+    for channel in range(3):
+        hist_item = cv2.calcHist([frame],[channel],None,[256],[0,255])
+        cv2.normalize(hist_item,hist_item,0,255,cv2.NORM_MINMAX)
+        hist += [hist_item]
+    return hist
+
+
+def correlation(hist1, hist2):
+    return min([cv2.compareHist(hist1[i], hist2[i], cv2.cv.CV_COMP_CORREL) for i in range(3)]) # min over all channels (colors)
+
+
+# grab only one keyframe per shot
 def get_keyframes(filename):
-    return []
+    vs = ffms.VideoSource(filename)
+    vs.set_output_format([ffms.get_pix_fmt('bgr24')])
+    width = vs.get_frame(0).EncodedWidth
+    height = vs.get_frame(0).EncodedHeight
+    keyframes = []
+    for i in range(len(vs.track.keyframes)-1):
+        img1 = keyframe_index(vs, width, height, i)
+        img2 = keyframe_index(vs, width, height, i+1)
+        hist1 = calc_histogram(img1)
+        hist2 = calc_histogram(img2)
+        correl = correlation(hist1, hist2)
+        if correl < SHOT_CORREL_THRESHOLD:
+            keyframes += [img1, img2]
+    if len(keyframes) == 0:
+        keyframes = [vs.get_frame(vs.track.keyframes[0]).planes[0].reshape((height, width, 3))]
+    return keyframes
 
 
 def discard_blanks(frames):
